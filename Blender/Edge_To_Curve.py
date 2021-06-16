51e5deddb0ac0dd8ee688b0ed7ce2f86938bcb69
Strømberg <Stromberg90@users.noreply.github.com>
2021-06-16 04:15:02 +0200

    Smooths curve on creation

--- Blender/Edge_To_Curve.py
+++ Blender/Edge_To_Curve.py
@@ -26,7 +26,7 @@ bl_info = {
     "wiki_url": "https://github.com/Stromberg90/Scripts/tree/master/Blender",
     "tracker_url": "https://github.com/Stromberg90/Scripts/issues",
     "blender": (2, 80, 0),
-    "version": (1, 0, 3)
+    "version": (1, 0, 4),
 }
 
 import bpy
@@ -39,39 +39,44 @@ class MeshMode:
 
 
 class ObjectMode:
-    OBJECT = 'OBJECT'
-    EDIT = 'EDIT'
-    POSE = 'POSE'
-    SCULPT = 'SCULPT'
-    VERTEX_PAINT = 'VERTEX_PAINT'
-    WEIGHT_PAINT = 'WEIGHT_PAINT'
-    TEXTURE_PAINT = 'TEXTURE_PAINT'
-    PARTICLE_EDIT = 'PARTICLE_EDIT'
-    GPENCIL_EDIT = 'GPENCIL_EDIT'
+    OBJECT = "OBJECT"
+    EDIT = "EDIT"
+    POSE = "POSE"
+    SCULPT = "SCULPT"
+    VERTEX_PAINT = "VERTEX_PAINT"
+    WEIGHT_PAINT = "WEIGHT_PAINT"
+    TEXTURE_PAINT = "TEXTURE_PAINT"
+    PARTICLE_EDIT = "PARTICLE_EDIT"
+    GPENCIL_EDIT = "GPENCIL_EDIT"
 
 
 class EventType:
-    MOUSEMOVE = 'MOUSEMOVE'
-    WHEELUPMOUSE = 'WHEELUPMOUSE'
-    WHEELDOWNMOUSE = 'WHEELDOWNMOUSE'
-    LEFTMOUSE = 'LEFTMOUSE'
-    RIGHTMOUSE = 'RIGHTMOUSE'
-    ESC = 'ESC'
+    MOUSEMOVE = "MOUSEMOVE"
+    WHEELUPMOUSE = "WHEELUPMOUSE"
+    WHEELDOWNMOUSE = "WHEELDOWNMOUSE"
+    LEFTMOUSE = "LEFTMOUSE"
+    RIGHTMOUSE = "RIGHTMOUSE"
+    ESC = "ESC"
+
 
 class ModalEdgeToCurve(bpy.types.Operator):
     bl_idname = "object.edge_to_curve"
     bl_label = "Edges To Curve"
     bl_description = "Takes selected mesh edges and converts them into a curve."
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = {"REGISTER", "UNDO"}
 
     @classmethod
     def poll(cls, context):
-        return context.active_object.mode == ObjectMode.EDIT and context.active_object.type == 'MESH' or context.active_object.type == 'CURVE'
+        return (
+            context.active_object.mode == ObjectMode.EDIT
+            and context.active_object.type == "MESH"
+            or context.active_object.type == "CURVE"
+        )
 
     def execute(self, context):
         context.object.data.bevel_depth = self.value / 100.0
         context.object.data.bevel_resolution = self.resolution
-        return {'FINISHED'}
+        return {"FINISHED"}
 
     def modal(self, context, event):
         if event.type == EventType.MOUSEMOVE:  # Apply
@@ -81,45 +86,45 @@ class ModalEdgeToCurve(bpy.types.Operator):
         elif event.type == EventType.WHEELDOWNMOUSE and self.resolution > 1:
             self.resolution -= 1
         elif event.type == EventType.LEFTMOUSE:  # Confirm
-            if context.active_object.type != 'CURVE':
+            if context.active_object.type != "CURVE":
                 context.view_layer.objects.active = self.original_object
-                bpy.ops.object.select_all(action='DESELECT')
+                bpy.ops.object.select_all(action="DESELECT")
                 self.original_object.select_set(True)
                 bpy.ops.object.mode_set(mode=ObjectMode.EDIT)
                 context.tool_settings.mesh_select_mode = MeshMode.EDGE
-            return {'FINISHED'}
+            return {"FINISHED"}
         elif event.type in {EventType.RIGHTMOUSE, EventType.ESC}:  # Cancel
-            if context.active_object.type == 'CURVE':
+            if context.active_object.type == "CURVE":
                 context.object.data.bevel_depth = 0
             else:
                 bpy.ops.object.delete()
                 context.view_layer.objects.active = self.original_object
-                bpy.ops.object.select_all(action='DESELECT')
+                bpy.ops.object.select_all(action="DESELECT")
                 self.original_object.select_set(True)
                 bpy.ops.object.mode_set(mode=ObjectMode.EDIT)
                 context.tool_settings.mesh_select_mode = MeshMode.EDGE
-            return {'CANCELLED'}
+            return {"CANCELLED"}
 
         self.execute(context)
-        return {'RUNNING_MODAL'}
+        return {"RUNNING_MODAL"}
 
     def invoke(self, context, event):
         self.value = 0.0
         self.start_value = event.mouse_x
         self.resolution = 2
         self.original_object = bpy.context.active_object
-        if context.active_object.type == 'CURVE':
-            context.object.data.fill_mode = 'FULL'
+        if context.active_object.type == "CURVE":
+            context.object.data.fill_mode = "FULL"
             context.object.data.bevel_resolution = self.resolution
             self.execute(context)
             context.window_manager.modal_handler_add(self)
-            return {'RUNNING_MODAL'}
+            return {"RUNNING_MODAL"}
 
         else:
             if context.tool_settings.mesh_select_mode[0]:
                 context.tool_settings.mesh_select_mode = MeshMode.EDGE
                 if not context.object.data.total_vert_sel > 1:
-                    return {'CANCELLED'}
+                    return {"CANCELLED"}
                 context.tool_settings.mesh_select_mode = MeshMode.VERTEX
 
             bpy.ops.mesh.duplicate()
@@ -127,20 +132,23 @@ class ModalEdgeToCurve(bpy.types.Operator):
             bpy.ops.object.mode_set(mode=ObjectMode.OBJECT)
             curve_object = context.selected_objects[-1]
             context.view_layer.objects.active = curve_object
-            bpy.ops.object.select_all(action='DESELECT')
+            bpy.ops.object.select_all(action="DESELECT")
             curve_object.select_set(True)
-            bpy.ops.object.convert(target='CURVE')
-            context.object.data.fill_mode = 'FULL'
+            bpy.ops.object.convert(target="CURVE")
+            context.object.data.fill_mode = "FULL"
+            bpy.ops.object.shade_smooth()
             context.object.data.bevel_resolution = self.resolution
             self.execute(context)
             context.window_manager.modal_handler_add(self)
-            return {'RUNNING_MODAL'}
+            return {"RUNNING_MODAL"}
+
 
 def EdgeToCurveMenuItem(self, context):
     layout = self.layout
     layout.separator()
-    layout.operator_context = 'INVOKE_DEFAULT'
+    layout.operator_context = "INVOKE_DEFAULT"
     layout.operator("object.edge_to_curve", text="Edges to Curve")
+
 
 def register():
     bpy.utils.register_class(ModalEdgeToCurve)
